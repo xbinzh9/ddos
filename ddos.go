eecf3f05f7a7a922748746cb2b250a1ef77b3f75
xbinzh9 <103558418+xbinzh9@users.noreply.github.com>
2022-08-25 19:22:24 +0700

    Update ddos.go

--- ddos.go
+++ ddos.go
@@ -195,7 +195,7 @@ func flood() {
 			s, err = net.Dial("tcp", addr)
 		}
 		if err != nil {
-			fmt.Println("Web sập chắc rồi muhahah!!!") //When showing this message, it means ur ip got blocked or the target server down.
+			fmt.Println("THANH BÌNH CUTE PHÔ MAI QUE") //When showing this message, it means ur ip got blocked or the target server down.
 		} else {
 			for i := 0; i < 100; i++ {
 				request := ""
